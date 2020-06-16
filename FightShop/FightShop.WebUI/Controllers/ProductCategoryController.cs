ff162efe19fcf2aadee9ff7820d481beeada477c
Bjorn Wann <bjorn.wann@gmail.com>
2020-06-16 23:52:30 +0200

    Unity dependency implementation

--- FightShop/FightShop.WebUI/Controllers/ProductCategoryController.cs
+++ FightShop/FightShop.WebUI/Controllers/ProductCategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FightShop.Core.Contracts;
 using FightShop.Core.Models;
 using FightShop.DataAccess.InMemory;
 
@@ -10,11 +11,11 @@ namespace FightShop.WebUI.Controllers
 {
     public class ProductCategoryController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryController()
+        public ProductCategoryController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
         // GET: ProductManager
         public ActionResult Index()
