73328c5bf5c9809194dfd06e3f1de97e7afee655
Bjorn Wann <bjorn.wann@gmail.com>
2020-06-19 13:27:50 +0200

    Basket model and db

---
+++ FightShop/FightShop.Core/Models/Basketitem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketD { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
