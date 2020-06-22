e338ce01913f5b31da503ba5bc8d61428de4b6d8
Bjorn Wann <bjorn.wann@gmail.com>
2020-06-23 00:23:01 +0200

    basket not working (db upgrade?)

---
+++ FightShop/FightShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using FightShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FightShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketService;
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+
+            return View();
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+            return View();
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
