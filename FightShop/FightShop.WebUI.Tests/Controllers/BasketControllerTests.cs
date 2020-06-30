bec93df0a4ffa86a6882bdf10bf835c4744e6efd
Bjorn Wann <bjorn.wann@gmail.com>
2020-06-30 03:13:01 +0200

    basket tests

---
+++ FightShop/FightShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using FightShop.Core.Contracts;
+using FightShop.Core.Models;
+using FightShop.Core.ViewModels;
+using FightShop.Services;
+using FightShop.WebUI.Controllers;
+using FightShop.WebUI.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FightShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            //setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new Basketservice(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+            //basketService.AddToBasket(httpContext, "1");
+            
+            //Act
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+
+        }
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = "10.00" });
+            products.Insert(new Product() { Id = "2", Price = "20.00" });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+
+            baskets.Insert(basket);
+
+            IBasketService basketService = new Basketservice(products, baskets);
+
+            var controller = new BasketController(basketService);
+            var httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            //Assert
+            //Assert.AreEqual(3, basketSummary.BasketCount);
+            //Assert.AreEqual(30.00, basketSummary.BasketTotal);            
+        }
+    }
+}
--- FightShop/FightShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ FightShop/FightShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -6,15 +6,29 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FightShop.WebUI;
 using FightShop.WebUI.Controllers;
+using FightShop.Core.Contracts;
+using FightShop.Core.Models;
+using FightShop.Core.ViewModels;
 
 namespace FightShop.WebUI.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class UnitTest1
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+            
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
             //// Arrange
             //HomeController controller = new HomeController();
 
@@ -23,32 +37,7 @@ namespace FightShop.WebUI.Tests.Controllers
 
             //// Assert
             //Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.About() as ViewResult;
-
-            //// Assert
-            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.Contact() as ViewResult;
-
-            //// Assert
-            //Assert.IsNotNull(result);
-        }
+            
+        }  
     }
 }
